b0aa4da246a042b95ae9eb20c9bb5bcd54d9b1ba
fbonnier <florent.bonnier@cnrs.fr>
2025-05-28 12:27:00 +0200

    WIP Fix  ignored patterns

--- main.py
+++ main.py
@@ -211,7 +211,7 @@ def generate_runscript_from_code (workdir=".", environment=None, pre_instruction
 
     # Start watchdog
     runscript_file.write("# Start Watchdog\n")
-    runscript_file.write("watchmedo shell-command --command='echo \"${watch_src_path} ${watch_dest_path}\" >> " + str(workdir) + "watchdog_log.txt' --patterns=\"*\" --ignore-patterns=\"watchdog_log.txt\" --ignore-directories --recursive " + str(outputs_folder_path) + " & WATCHDOG_PID=$!;\n\n")
+    runscript_file.write("watchmedo shell-command --command='echo \"${watch_src_path} ${watch_dest_path}\" >> " + str(workdir) + "watchdog_log.txt' --patterns=\"*\" --ignore-patterns=watchdog_log.txt;run_stderr.txt;run_stdout.txt --ignore-directories --recursive " + str(outputs_folder_path) + " & WATCHDOG_PID=$!;\n\n")
 
     # Run
     runscript_file.write("# RUN\n")
